abd5804b272f186bbd9951b263842e860983b6d9
RaF1KGame <cyclik134reg@gmail.com>
2023-09-12 16:54:59 +0300

    Lesson 1

---
+++ .top.menu.php
@@ -0,0 +1,46 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Компания", 
+		"company/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Новости", 
+		"news/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Каталог", 
+		"products/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Акции", 
+		"Sale/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Партнерам", 
+		"/partneram/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Контакты", 
+		"contacts/", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
---
+++ partneram/raspisanie-meropriyatiy/index.php
@@ -0,0 +1,7 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetPageProperty("title", "Расписание мероприятий");
+$APPLICATION->SetPageProperty("keywords", "Расписание мероприятий");
+$APPLICATION->SetPageProperty("description", "Расписание мероприятий");
+$APPLICATION->SetTitle("Расписание мероприятий");
+?>Text here....<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
